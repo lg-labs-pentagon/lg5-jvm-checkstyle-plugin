27b6790c206af5e14f09a2912d4cacb2074462a1
Luis Fernando García Q... <105936384+lglabs@users.noreply.github.com>
2024-04-19 11:43:00 +0200

    [LG-41]- clean Plugins unnecessary

--- Makefile
+++ Makefile
@@ -4,5 +4,8 @@ hooks-pre-push:
 	cd .git/hooks && cp pre-push.sample pre-push
 	cd .git/hooks && ln -s -f ../../hooks/pre-push pre-push
 
-
+checkstyle-build:
+	./gradlew build
+checkstyle-test:
+	./gradlew test
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,17 +2,16 @@ plugins {
     kotlin("jvm") version "1.9.23"
     id("java-gradle-plugin")
     id("com.gradle.plugin-publish") version "1.2.1"
-    checkstyle
-    pmd
 }
 
 gradlePlugin {
-    website.set("https://github.com/lg-labs-pentagon/lg5-spring/lg5-jvm-checkstyle")
-    vcsUrl.set("https://github.com/lg-labs-pentagon/lg5-spring/lg5-jvm-checkstyle")
+
+    website.set("https://github.com/lg-labs-pentagon/lg5-jvm-checkstyle-plugin")
+    vcsUrl.set("https://github.com/lg-labs-pentagon/lg5-jvm-checkstyle-plugin")
     plugins {
         create("gradleCodeStylePluginExample") {
             id = "io.github.lg5.jvm.checkstyle.code"
-            displayName = "Gradle Plugin Code Style Example"
+            displayName = "Gradle Plugin Code Style"
             description = "Predefined Checkstyle and PMD rules"
             implementationClass = "io.github.lg5.jvm.checkstyle.code.CodingRulesGradlePlugin"
             tags.set(listOf("codestyle", "checkstyle", "pmd"))
